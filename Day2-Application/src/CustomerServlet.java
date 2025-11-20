a0714563ff2e65fc49fadbc4997f9528177b9183
Udara San <udarassanjeewa@gmail.com>
2025-11-20 15:21:38 +0530

    get method implementation updated

--- Day2-Application/src/CustomerServlet.java
+++ Day2-Application/src/CustomerServlet.java
@@ -24,12 +24,13 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
         for(Customer c:customerList){
-            System.out.println(
-                    "CID :"+c.getId()
-                    +"CName :"+ c.getName()
-                    +"CAddress :"+c.getAddress()
-            );
+            resp.getWriter().println("<tr>" +
+                    "<td>"+c.getId()+"</td>" +
+                    "<td>"+c.getName()+"</td>" +
+                    "<td>"+c.getAddress()+"</td>" +
+                    "</tr>");
         }
     }
 
