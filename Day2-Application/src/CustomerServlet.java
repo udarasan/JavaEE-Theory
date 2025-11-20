e835705871e1b77e1fedf454c50df517ef4f7c6c
Udara San <udarassanjeewa@gmail.com>
2025-11-20 14:54:27 +0530

    get method implementation

--- Day2-Application/src/CustomerServlet.java
+++ Day2-Application/src/CustomerServlet.java
@@ -11,13 +11,26 @@ import java.util.List;
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
 
+    List<Customer> customerList=new ArrayList<Customer>();
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id=req.getParameter("id");
         String name=req.getParameter("name");
         String address=req.getParameter("address");
         System.out.println(id+name+address);
-        List<Customer> customerList=new ArrayList<Customer>();
         customerList.add(new Customer(id,name,address));
     }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        for(Customer c:customerList){
+            System.out.println(
+                    "CID :"+c.getId()
+                    +"CName :"+ c.getName()
+                    +"CAddress :"+c.getAddress()
+            );
+        }
+    }
+
 }
